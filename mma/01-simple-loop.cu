5d9ea7abfd77439681f13ffa4fe33a653babd485
abnerhexu <hexu@rochexp.com>
2025-01-06 13:03:52 +0800

    add gitignore; change mm to mma

--- mma/01-simple-loop.cu
+++ mma/01-simple-loop.cu
@@ -11,7 +11,9 @@ __global__ void simple_loop_mm(float* a, float* b, float* c, int M, int K, int N
         for (int k = 0; k < K; k++) {
             sum += a[row * K + k] * b[k * N + col];
         }
-        c[row * N + col] = sum;
+        // 注意，在这里可能c矩阵同一个位置被写入多次，但是我们并不在乎，因为最终结果是一样的
+        // 原因在于可能有多个元素都满足row < M并且col < N
+        c[row * N + col] += sum;
     }
 }
 
@@ -22,7 +24,7 @@ void standard_model_mm(float* a, float* b, float* c, int M, int K, int N) {
             for (int k = 0; k < K; k++) {
                 sum += a[row * K + k] * b[k * N + col];
             }
-            c[row * N + col] = sum;
+            c[row * N + col] += sum;
         }
     }
 }
@@ -32,16 +34,17 @@ int main() {
     int K = 1024;
     int N = 1024;
 
-    auto a = generate_random_array<float>(M * K, -1.0, 1.0);
-    auto b = generate_random_array<float>(K * N, -1.0, 1.0);
-    auto c = generate_random_array<float>(M * N, -1.0, 1.0);
+    auto a = generate_random_array<float>(M * K, 1.0, 1.0);
+    auto b = generate_random_array<float>(K * N, 1.0, 1.0);
+    auto c = generate_random_array<float>(M * N, 1.0, 1.0);
+
+    std::vector<float> cc;
+    std::copy(c.begin(), c.end(), std::back_inserter(cc));
 
     // Call the standard model
     standard_model_mm(a.data(), b.data(), c.data(), M, K, N);
 
     // Initialize matrices a, b, and c
-    std::vector<float> cc;
-    cc.assign(c.begin(), c.end());
     float* d_a, * d_b, * d_c;
     cudaMalloc((void**)&d_a, M * K * sizeof(float));
     cudaMalloc((void**)&d_b, K * N * sizeof(float));
@@ -51,8 +54,8 @@ int main() {
     cudaMemcpy(d_c, cc.data(), M * N * sizeof(float), cudaMemcpyHostToDevice);
 
     // Call the GPU model
-    dim3 blockDim(4, 4, 1);
-    dim3 gridDim(8, 8, 1);
+    dim3 blockDim(32, 32, 1);
+    dim3 gridDim(32, 32, 1);
     simple_loop_mm<<<gridDim, blockDim>>>(d_a, d_b, d_c, M, K, N);
 
     // Copy the result back to the host
