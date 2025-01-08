30a8c02723ccb7bb6b673cba87aae6a2f33a46c0
abnerhexu <hexu@rochexp.com>
2025-01-08 15:43:39 +0800

    fixed sync bug in mma/03, add mma/04 using float4

--- mma/04-tiled-float4.cu
+++ mma/04-tiled-float4.cu
@@ -2,31 +2,39 @@
 #include <cuda.h>
 #include "../utils/timer.h"
 #include "../utils/data.h"
+#include <assert.h>
 
-// blockDim.x = 32, blockDim.y = 32
-template<int BM, int BN, int BK, int TM, int TN> // TM = TN = 4, BK = 8; BM = blockDim.x*TM = 128, BN = blockDim.y*TN = 128
+// blockDim.x = 16, blockDim.y = 16
+template<int BM, int BN, int BK, int TM, int TN> // TM = TN = 8, BK = 8; BM = blockDim.x*TM = 128, BN = blockDim.y*TN = 128
 __global__ void tiled_mma(float *a, float *b, float *c, int M, int N, int K) {
     __shared__ float tiled_a[BM * BK]; // 128 * 8 
-    __shared__ float tiled_b[BK * BN]; // 8 * 128
-    float tmp[TM * TN] = {0}; // finally, compute a 4 * 4 block **every thread**
+    __shared__ float tiled_b[BN * BK]; // 8 * 128, but transposed
+    float tmp[TM * TN] = {0.0f}; // finally, compute a 4 * 4 block **every thread**
     int tid = threadIdx.x + threadIdx.y * blockDim.x; // threaed id in a block, assert it >= 128*8
-    int tiled_ax = tid % 8;
-    int tiled_ay = tid / 8;
-    int tiled_bx = tid % 128;
-    int tiled_by = tid / 128;
-    int tiled_aidx = tiled_ay * BK + tiled_ax;
-    int tiled_bidx = tiled_by * BN + tiled_bx;
+    int tiled_ax = tid % 2;
+    int tiled_ay = tid / 2;
+    int tiled_bx = tid % 32;
+    int tiled_by = tid / 32;
+    int tiled_aidx = tiled_ay * BK + tiled_ax * 4;
+    int tiled_bidx = tiled_bx * BK * 4 + tiled_by;
     int ay = TM * blockIdx.y * blockDim.y;
     int bx = TN * blockIdx.x * blockDim.x;
+    float pb[4];
     for (int it = 0; it < K; it += BK) {
-        tiled_a[tiled_aidx] = a[(ay + tiled_ay) * K + it + tiled_ax];
-        tiled_b[tiled_bidx] = b[(tiled_by + it) * N + bx + tiled_bx];
+        // tiled_a[tiled_aidx] = a[(ay + tiled_ay) * K + it + tiled_ax];
+        // tiled_b[tiled_bidx] = b[(tiled_by + it) * N + bx + tiled_bx];
+        (float4 &)tiled_a[tiled_aidx] = (float4 &)a[(ay + tiled_ay) * K + it + 4 * tiled_ax];
+        (float4 &)pb[0] = (float4 &)b[(tiled_by + it) * N + bx + 4 * tiled_bx];
+        tiled_b[tiled_bidx         ] = pb[0];
+        tiled_b[tiled_bidx + BK    ] = pb[1];
+        tiled_b[tiled_bidx + 2 * BK] = pb[2];
+        tiled_b[tiled_bidx + 3 * BK] = pb[3];
+        // (float4 &)tiled_b[tiled_bidx] = (float4 &)b[(tiled_by + it) * N + bx + 4 * tiled_bx];
         __syncthreads();
-        // 现在tiled_a由1024个线程加载了128*8个数据，tield_b由1024个线程加载了8*128个数据
         for (int p = 0; p < TM; p++) {
             for (int q = 0; q < TN; q++) {
                 for (int k = 0; k < BK; k++) {
-                    tmp[p * TN + q] += tiled_a[(p + threadIdx.y * TM) * BK + k] * tiled_b[k * BN + threadIdx.x * TN + q];
+                    tmp[p * TN + q] += tiled_a[(p + threadIdx.y * TM) * BK + k] * tiled_b[(q + threadIdx.x * TN) * BK + k];
                 }
             }
         }
@@ -56,9 +64,9 @@ int main() {
     int K = 1024;
     int N = 1024;
 
-    auto a = generate_random_array<float>(M * K, -1.0, 1.0);
-    auto b = generate_random_array<float>(K * N, -1.0, 1.0);
-    auto c = generate_random_array<float>(M * N, -1.0, 1.0);
+    auto a = generate_random_array<float>(M * K, 1.0, 1.0);
+    auto b = generate_random_array<float>(K * N, 1.0, 1.0);
+    auto c = generate_random_array<float>(M * N, 1.0, 1.0);
 
     std::vector<float> cc;
     std::copy(c.begin(), c.end(), std::back_inserter(cc));
@@ -77,11 +85,11 @@ int main() {
 
     // Call the GPU model
     dim3 blocksPerGrid(8, 8, 1);
-    dim3 threadsPerBlock(32, 32, 1);
+    dim3 threadsPerBlock(16, 16, 1);
     CUDAProgTimer timer;
     timer.start();
     for (int i = 0; i < 1; i++)
-    tiled_mma<128, 128, 8, 4, 4><<<blocksPerGrid, threadsPerBlock>>>(d_a, d_b, d_c, M, K, N);
+    tiled_mma<128, 128, 8, 8, 8><<<blocksPerGrid, threadsPerBlock>>>(d_a, d_b, d_c, M, K, N);
     timer.stop();
     timer.info();
 
@@ -96,5 +104,13 @@ int main() {
     
     // Result check
     assert_close<float>(c.data(), cc.data(), M * N, 1e-5);
+    // for (int p = 0; p < 1024; p++) {
+    //     for (int q = 0; q < 1024; q++) {
+    //         // std::cout << cc[p * 1024 + q] << " ";
+    //         assert(cc[p * 1024 + q] == 1);
+    //     }
+    //     // std::cout << std::endl;
+    // }
+    
     return 0;
 }
